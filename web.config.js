009b153a7149fa652fa40786ecc18d821912162d
RenBlueSky <3530184262@qq.com>
2018-11-02 00:13:13 +0800

    node.js¹ÜÀíºǫ́

---
+++ web.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    DbConnectionString: "mssql://sa:123456@127.0.0.1:1433/AuthPublish",
+    MySqlConnectionCfg: {
+        host: "127.0.0.1",
+        user: 'root',
+        password: '123456',
+        port: '3306',
+        database: 'db'
+    },
+    TokenSecret: "msg is a handsome boy",
+    HttpPort: 3000,
+    WebsocketPort: 5000
+};
